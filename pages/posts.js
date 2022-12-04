24504b9cd224b7f80bead6a85f7602457048b16b
AndriyBats <84588992+AndriyBats@users.noreply.github.com>
2022-12-04 23:55:55 +0200

    commit

---
+++ pages/posts.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Posts () {
+    return (
+        <>
+          <Head>
+            <title>Post Page || Next Course</title>
+          </Head>
+         <h1>Posts page</h1>
+         <p>Lorem ipsum kwhkjkjx dxidhkj xwjxwjxw  weoijdoiewj d jwoidjoew dewj djoewjdo woi dowoi cdew oic</p>
+        </>
+    )
+}
